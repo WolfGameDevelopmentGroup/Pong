20b7abf4ca2bffd1daeaaf832970d86efb10a261
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-05-30 16:21:36 -0300

    Factory to build Player and Enemy classes #4
    Player and Enemy classes will be Build through a PlayersFactory class.
    This startegy is based on Factory method design pattern.

---
+++ TestPlayersFactory.java
@@ -0,0 +1,24 @@
+/*
+	 TestPlayersFactory.java (Java)
+	 
+	 Purpose: Test of PlayersFactory class to build Players of the game.
+	 
+	 Site: https://dirack.github.io
+	 
+	 Version 1.0
+	 
+	 Programer: Rodolfo A C Neves (Dirack) 30/05/2020
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+import junit.framework.*;
+
+public class TestPlayersFactory extends TestCase{
+
+	public void testPlayersBuilder(){
+		Player = new PlayersFactory();
+	}
+}
