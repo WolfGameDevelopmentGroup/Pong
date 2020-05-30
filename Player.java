3d3f9d8bdd4c9911cea89ec51bf17821916ca487
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-05-30 17:04:54 -0300

    Implements Player class #4
    Player is the rectangle controled by user in the Pong Player game

---
+++ Player.java
@@ -0,0 +1,22 @@
+/*
+	 Player.java (Java)
+	 
+	 Purpose: Player of the game. Controled by user.
+	 
+	 Site: https://dirack.github.io
+	 
+	 Version 1.0
+	 
+	 Programer: Rodolfo A C Neves (Dirack) 30/05/2020
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+public class Player implements PongPlayer{
+	
+	public void hello(){
+		System.out.println("Hello, I'm the Player!");
+	}
+}
